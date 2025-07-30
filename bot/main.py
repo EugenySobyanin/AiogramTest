22b846a3fff92827de6aaf329e7e1b9569f15294
ea.sobyanin2018@gmail.com <PC1013487026@DTDF.ru>
2025-07-30 17:50:24 +0300

    Начало тестового бота.

---
+++ bot/main.py
@@ -0,0 +1,17 @@
+from aiogram import Bot, Dispatcher, executor, types
+
+
+TOKEN_API = ''
+
+bot = Bot(TOKEN_API)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(text=message.text)  # написать сообщение
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
