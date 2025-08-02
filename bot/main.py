5ba4da4c1ab4d718a53395f66b8e2191f9aa568e
Евгений Собянин <ea.sobyanin2018@gmail.com>
2025-08-02 22:13:35 +0300

    Начало активной работы.

--- bot/main.py
+++ bot/main.py
@@ -1,17 +1,35 @@
-from aiogram import Bot, Dispatcher, executor, types
+import asyncio
+import os
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+from dotenv import find_dotenv, load_dotenv
 
 
-TOKEN_API = ''
+load_dotenv(find_dotenv())
 
-bot = Bot(TOKEN_API)
-dp = Dispatcher(bot)
+bot = Bot(token=os.getenv('TOKEN'))
+dp = Dispatcher()
 
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(text=message.text)  # написать сообщение
+@dp.message(CommandStart())
+async def start_cmd(message: types.Message):
+    await message.answer('Это была команда старт.')
 
+
+@dp.message()
+async def echo(message: types.Message, bot: Bot):
+    await bot.send_message(message.from_user.id, 'Ответ')
+    await message.reply('Гоу эвей.')
+    await message.answer(message.text)
+
+
+async def main():
+    await ...
+    await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
